540cec20bfbde1e0494bcdfa5d7a2dd4a4968bf0
Santosh5924 <85626061+Santosh5924@users.noreply.github.com>
2022-12-30 23:30:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function show(){
+  var text1 = document.getElementById('text1');
+  var text2 = document.getElementById('text2');
+  var text2 = document.getElementById('text2');
+  if(text1.value === '' || text2.value === '' || text3.value === ''){
+
+  }
+  else{
+    alert("Form Submitted Successfully");
+  }
+}
